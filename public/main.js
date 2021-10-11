26a2bc3d81ec6f76dc0107df1802ac722c5e4c16
aldiwira <aldiwira.aw@gmail.com>
2021-10-11 17:30:57 +0700

    fixing electron export

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+const url = require("url");
 
 function createWindow() {
   // Create the browser window.
@@ -8,18 +9,20 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    title: 'Travling',
+    title: "Travling",
     webPreferences: {
+      webSecurity: false,
       nodeIntegration: true,
     },
   });
 
-  // if (isDev) {
-  //   //load the index.html from a url
-  //   win.loadURL('http://localhost:3000');
-  // } else {
-  // }
-  win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 }
 
 // This method will be called when Electron has finished
@@ -30,13 +33,13 @@ app.whenReady().then(createWindow);
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
 
